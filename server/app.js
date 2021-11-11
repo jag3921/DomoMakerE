bd3a2d6abe4fe75015e888298d552fccec0043ad
jag3921 <jag3921@rit.edu>
2021-11-11 17:48:32 -0500

    domomaker d

---
+++ server/app.js
@@ -0,0 +1,92 @@
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const csrf = require('csurf');
+const redis = require('redis');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
+
+const dbURL = 'mongodb+srv://jacobUser:jacobPassword0@cluster0.tzvux.mongodb.net/DomoMaker';
+mongoose.connect(dbURL, (err) => {
+  if (err) {
+    //   console.log('Could not connect to database');
+    throw err;
+  }
+});
+
+let redisURL = {
+  hostname: 'redis-14746.c16.us-east-1-3.ec2.cloud.redislabs.com',
+  port: '14746',
+};
+
+let redisPass = 'GQwKR9aGQ64RecqOov5VC9J9fU8cx4K8';
+
+if (process.env.REDISCLOUD_URL) {
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  [, redisPass] = redisURL.auth.split(':');
+}
+
+const redisClient = redis.createClient({
+  host: redisURL.hostname,
+  port: redisURL.port,
+  password: redisPass,
+});
+// Pull in our routes
+const router = require('./router.js');
+
+// App
+const app = express();
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.use(compression());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.use(session({
+  key: 'sessionid',
+  store: new RedisStore({
+    client: redisClient,
+  }),
+  secret: 'Domo Arigato',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+  },
+}));
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/../views`);
+app.disable('x-powered-by');
+app.use(cookieParser());
+
+// csrf must come AFTER app.us(cookieParser())
+// and app.use(session)
+// should come BEFORE the router
+
+app.use(csrf());
+app.use((err, req, res, next) => {
+  if (err.code !== 'EBADCSRFTOKEN') return next(err);
+
+ // console.log('Missing CSRF token');
+  return false;
+});
+
+router(app);
+
+app.listen(port, (err) => {
+  if (err) {
+    throw err;
+  }
+  // console.log(`Listening on port ${port}`);
+});
